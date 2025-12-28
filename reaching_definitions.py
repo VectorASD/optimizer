f88996f147859ef5f0e022b63be8ea6d4b790075
VectorASD <vectorasd@vk.com>
2025-12-28 23:13:53 +0700

    AE (available_expressions) реализован окончательно!

--- reaching_definitions.py
+++ reaching_definitions.py
@@ -167,7 +167,7 @@ def bits_to_defs(definitions, mask):
             out.append(definitions[i])
         mask >>= 1
         i += 1
-    return ", ".join(f"({d[0]}, {d[1]})" for d in out)
+    return ", ".join(f"({', '.join(map(str, d))})" for d in out)
 
 
 
@@ -176,7 +176,7 @@ def reaching_definitions(BB_F, debug=False):
     if debug: 
         print("blocks:", pformat(blocks))
         print("preds:",  pformat(preds))
-        print("succs:",  pformat(succs)) # UNUSED
+        # print("succs:",  pformat(succs)) UNUSED
         print("~" * 77)
 
     definitions = []
@@ -192,11 +192,13 @@ def reaching_definitions(BB_F, debug=False):
                     seen.add(var)
         local_defs.reverse()
         definitions.extend(local_defs)
-    if debug:
-        print("definitions:", pformat(definitions))
 
   # GEN, KILL = bad_gen_kill_maker(blocks, definitions)
     GEN, KILL = gen_kill_maker(blocks, definitions)
+    if debug:
+        print("definitions:", pformat(definitions))
+        print("GEN:", pformat(GEN))
+        print("KILL:", pformat(KILL))
 
     all_bits = (1 << len(definitions)) - 1
     notKILL = {bb: (~KILL[bb]) & all_bits for bb in blocks}
@@ -227,7 +229,98 @@ def reaching_definitions(BB_F, debug=False):
 
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ AE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def AE_gen_kill_maker(blocks):
+    block_exprs = defaultdict(int) # выражения, вычисленные в блоке
+    block_kills = defaultdict(set) # переменные, переопределённые в блоке
+    expr_index = {} # expr → bit
+    expr_mask = 1
+    for bb, ops in blocks.items():
+        for op in ops:
+            if op[0] == 1: # <var> = <var|num> <+|-|*|/|%> <var|num>
+                expr = op[2:5] # (lhs, op, rhs)
+                if expr not in expr_index:
+                    expr_index[expr] = expr_mask
+                    expr_mask <<= 1
+                block_exprs[bb] |= expr_index[expr]
+                block_kills[bb].add(op[1]) # <op[1]> = ...
+
+    expressions = tuple(expr_index)
+    GEN = dict(block_exprs)
+
+    uses = defaultdict(int) # преобразует переменную в битовую маску всех выражений, где она юзается
+    for expr, bit in expr_index.items():
+        lhs, _, rhs = expr
+        if isinstance(lhs, str): uses[lhs] |= bit
+        if isinstance(rhs, str): uses[rhs] |= bit
+    KILL = {}
+    # KILL[B] = битовая маска всех выражений, которые становятся недоступны,
+    # потому что в блоке B переопределяются переменные, УЧАВСТВУЮЩИЕ в этих выражениях
+    for bb in blocks:
+        bits = 0
+        for v in block_kills[bb]: bits |= uses[v]
+        KILL[bb] = bits
+
+    return expressions, GEN, KILL
+
+
+
+def available_expressions(BB_F, debug=False): 
+    blocks, preds, succs = BB_F
+    if debug: 
+        print("blocks:", pformat(blocks))
+        print("preds:",  pformat(preds))
+        # print("succs:",  pformat(succs)) UNUSED
+        print("~" * 77)
+
+    expressions, GEN, KILL = AE_gen_kill_maker(blocks)
+    if debug:
+        print("expressions:", pformat(expressions))
+        print("GEN:", pformat(GEN))
+        print("KILL:", pformat(KILL))
+
+    all_bits = (1 << len(expressions)) - 1
+    notKILL = {bb: (~KILL[bb]) & all_bits for bb in blocks}
+    # all_bits нужен для ускорения &-операций с бесконечной длиной единичек слева
+
+    # RIN  = {bb: 0 for bb in blocks} фундаментальная ошибка. Раз meet вместо join, то заменяем bottom на top
+    entry = next(iter(blocks))
+    RIN = {bb: all_bits for bb in blocks} # all_bits оказался этим самым top, хоть и создан вообще не для этого
+    RIN[entry] = 0 # top -> bottom для ПЕРВОГО блока
+    ROUT = RIN.copy()
+    print(f"AVIN(init): {RIN}")
+    print(f"AVOUT(init): {ROUT}")
+
+    changed = True
+    while changed:
+        changed = False
+        for bb in blocks: # (Python 3.7+) порядок ключей во время вставки/объявления сохраняется!
+            if preds[bb]:
+                rin = all_bits
+                for p in preds[bb]: rin &= ROUT[p] # вспоминаем опять meet
+            else: rin = 0
+
+            rout = GEN[bb] | (rin & notKILL[bb]) # ради чего весь этот переход set на int
+
+            if rin != RIN[bb] or rout != ROUT[bb]:
+                RIN[bb], ROUT[bb], changed = rin, rout, True
+
+    if debug:
+        for bb in RIN:
+            print()
+            print(f"AVIN({bb}): {bits_to_defs(expressions, RIN[bb])}")
+            print(f"AVOUT({bb}): {bits_to_defs(expressions, ROUT[bb])}")
+
+    return expressions, GEN, KILL, RIN, ROUT
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 program_0 = """
 BB0: x = 10;
@@ -244,6 +337,31 @@ BB2: t = x + y
      goto BB0; // (t, BB2) расползается по всему коду, если не сделать return терминатором
 """
 
+program_1 = """
+BB0: y = x + 2;
+     x = a + b;
+     x = x + y;
+     goto BB1;
+// AVIN: {a + b}
+BB1: y = x + 2;
+     t = x - 1;
+     if (y > t) goto BB3; else goto BB2;
+// AVIN: {a + b, x + 2, x - 1}
+BB2:
+     y = x - 1;
+     goto BB3;
+// AVIN: {a + b, x + 2, x - 1}
+BB3: t = x + 2;
+     if (t > 0) goto BB1; else goto BB4;
+// AVIN: {a + b, x + 2, x - 1}
+BB4: t = a + b;
+     return t;
+"""
+
 if __name__ == "__main__":
     BB_F = parse_program(program_0)
     reaching_definitions(BB_F, debug=True)
+    print("~" * 77)
+    BB_F = parse_program(program_1)
+    available_expressions(BB_F, debug=True)
+
